173220dab9fcc8cc105e49cad0dd710ff9b9b1f7
Marko Radojevic <marko.d.radojevic@gmail.com>
2023-09-21 22:17:12 +0200

    .

--- ports/mylibrary/portfile.cmake
+++ ports/mylibrary/portfile.cmake
@@ -1,8 +1,6 @@
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
-	REPO Murk42/MyLibrary
-	REF 505ef9ba1c201aeeace1cb71bbc2ee833540dfa6 #hash to the library repo commit 
-	SHA512 eb3e594080686a055b383fe9416c5942a3edae8a7637c54294ea53079685d5f2c20d150e10d37eb028bd5c823a24fea4004b045ad9133dfac0b6973c7cf8ef8a
+	REPO Murk42/MyLibrary	
 	HEAD_REF master
 )
 
